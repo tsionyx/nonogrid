5c2a14f41b9b0db007f4c6b61641b56f1fffa69c
Ivan Ladelshchikov <tsionyx@gmail.com>
2019-03-07 13:43:52 +0500

    add integration tests

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod board;
+pub mod parser;
+pub mod render;
+pub mod solver;
+mod utils;
+
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate log;
---
+++ tests/solving.rs
@@ -0,0 +1,59 @@
+use nonogrid::{
+    board::{BinaryBlock, BinaryColor},
+    parser::{BoardParser, MyFormat},
+    solver::{line, probing, propagation},
+};
+
+#[macro_use]
+extern crate log;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[test]
+fn hello() {
+    let board = MyFormat::<BinaryBlock>::read_board("examples/hello.toml");
+    let board = Rc::new(RefCell::new(board));
+
+    warn!("Solving with simple line propagation");
+    let solver = propagation::Solver::new(Rc::clone(&board));
+    solver.run::<line::DynamicSolver<_>>().unwrap();
+
+    let board = board.borrow();
+
+    assert!(board.is_solved_full());
+    assert_eq!(board.solution_rate(), 1.0);
+
+    let b = BinaryColor::Black;
+    let w = BinaryColor::White;
+    assert_eq!(board.get_column(0), vec![b; 7]);
+    assert_eq!(
+        board.get_column(board.width() - 1),
+        vec![b, b, b, b, b, w, b]
+    );
+}
+
+#[test]
+fn pony() {
+    let board = MyFormat::<BinaryBlock>::read_board("examples/MLP.toml");
+    let board = Rc::new(RefCell::new(board));
+
+    warn!("Solving with simple line propagation");
+    let solver = propagation::Solver::new(Rc::clone(&board));
+    solver.run::<line::DynamicSolver<_>>().unwrap();
+
+    {
+        let board = board.borrow();
+        assert!(!board.is_solved_full());
+        assert_eq!(board.solution_rate(), 0.0);
+    }
+
+    let solver = probing::FullProbe1::new(Rc::clone(&board));
+    solver.run::<line::DynamicSolver<_>>().unwrap();
+
+    {
+        let board = board.borrow();
+        assert!(board.is_solved_full());
+        assert_eq!(board.solution_rate(), 1.0);
+    }
+}
