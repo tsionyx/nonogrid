7037a22f9bd0f64d98f31d5a99087cc5ed5b1d30
Ivan Ladelshchikov <tsionyx@gmail.com>
2021-01-15 15:33:58 +0500

    reformat compiler lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,41 @@
-// The list was generated with the command
+//! The `nonogrid` contains set of algorithms and solvers to solve the nonogram puzzles.
+
+// do not warn on older Rust versions
+#![allow(unknown_lints)]
+//
+// The following list was generated with the command
 //   $ rustc -W help | grep ' allow ' | awk '{print $1}' | tr - _ | sort | xargs -I{} echo '#![warn({})]'
+//
 #![warn(absolute_paths_not_starting_with_crate)]
 #![warn(anonymous_parameters)]
-// #![warn(box_pointers)]
+// use `Box` without fear
+#![allow(box_pointers)]
 #![warn(deprecated_in_future)]
-// #![warn(elided_lifetimes_in_paths)]
+#![warn(elided_lifetimes_in_paths)]
 #![warn(explicit_outlives_requirements)]
 #![warn(indirect_structural_match)]
+#![warn(invalid_html_tags)]
 #![warn(keyword_idents)]
 #![warn(macro_use_extern_crate)]
 #![warn(meta_variable_misuse)]
 #![warn(missing_copy_implementations)]
+#![warn(missing_crate_level_docs)]
 #![warn(missing_debug_implementations)]
 #![warn(missing_doc_code_examples)]
-//#![warn(missing_docs)]  // TODO
 #![warn(non_ascii_idents)]
+#![warn(pointer_structural_match)]
 #![warn(private_doc_tests)]
 #![warn(single_use_lifetimes)]
 #![warn(trivial_casts)]
 #![warn(trivial_numeric_casts)]
-// #![warn(unreachable_pub)] // TODO
+#![warn(unaligned_references)]
+// conflicts with the `clippy::redundant_pub_crate`
+#![allow(unreachable_pub)]
+// !!! NO UNSAFE
+#![forbid(unsafe_code)]
 #![warn(unstable_features)]
+// some crates are only used in binary (see `main.rs`), not in the lib itself
+#![allow(unused_crate_dependencies)]
 #![warn(unused_extern_crates)]
 #![warn(unused_import_braces)]
 #![warn(unused_labels)]
@@ -28,17 +43,19 @@
 #![warn(unused_qualifications)]
 #![warn(unused_results)]
 #![warn(variant_size_differences)]
-// recommendations
-#![forbid(unsafe_code)]
+//
+// additional recommendations
 #![deny(clippy::mem_forget)]
-// suppress some pedantic warnings
-#![allow(clippy::non_ascii_literal)]
-#![allow(clippy::must_use_candidate)]
+// `use super::*` in tests
 #![cfg_attr(test, allow(clippy::wildcard_imports))]
-// TODO: remove the following suppression:
+//
+//
+// FIXME: remove or localize the following suppression:
+#![allow(missing_docs)]
 #![allow(clippy::cast_precision_loss)]
-#![allow(clippy::module_name_repetitions)]
 #![allow(clippy::missing_errors_doc)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::must_use_candidate)]
 
 pub use self::{
     block::{
